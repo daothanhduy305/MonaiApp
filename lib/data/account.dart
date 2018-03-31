4f90ff3bda35a62df18a025fcef5f8392bef814f
ebolo <daothanhduy305@gmail.com>
2018-03-31 14:13:33 +0700

    Add icon to todo list for account
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- lib/data/account.dart
+++ lib/data/account.dart
@@ -1,6 +1,7 @@
 import 'package:monai/data/currency.dart';
 
 class Account {
+  // TODO: There might be icon in the future
   String name;
   double initialBalance, currentBalance;
   Currency currency;
