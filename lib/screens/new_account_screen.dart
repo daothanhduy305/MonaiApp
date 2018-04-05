66d1fc6bea1bf65808bd2bf4e7c8ee4a44c37a00
ebolo <daothanhduy305@gmail.com>
2018-04-05 20:48:27 +0700

    Update currency on chosen Fix keyboard type for textfields
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- lib/screens/new_account_screen.dart
+++ lib/screens/new_account_screen.dart
@@ -22,48 +22,65 @@ class NewAccountScreenState extends State<NewAccountScreen> {
         children: <Widget>[
           new ListTile(
             title: new TextField(
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(hintText: 'Name'),
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(labelText: 'Name'),
             ),
           ),
           new ListTile(
             title: new TextField(
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(hintText: 'Category'),
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(labelText: 'Category'),
             ),
           ),
-          new Row(
-            children: <Widget>[
-              new Expanded(child: new ListTile(
-                title: new TextField(
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Expanded(child: new TextField(
                   keyboardType: TextInputType.number,
                   decoration: new InputDecoration(
-                    hintText: currentCurrency == null ? '' : '0.0'),
-                ),
-                trailing: currentCurrency == null ? null : new Text(
-                  currentCurrency.shortName),
-              ),),
-              new Builder(builder: (context) =>
-              new FlatButton(
-                child: new Text(
-                  currentCurrency == null ? 'Currency' : currentCurrency
-                    .shortName),
-                onPressed: () {
-                  CurrencyProvider.getInstance().getAllCurrencies().then((
-                    currencies) =>
-                    showModalBottomSheet(context: context, builder: (context) =>
-                    new Container(
-                      child: new ListView(
-                        padding: new EdgeInsets.only(top: 10.0),
-                        children: currencies.map((currency) =>
-                        new ListTile(title: new Text(currency.longName),))
-                          .toList(),
-                      ),
-                    )));
-                },
-              ))
-            ],
-          ),
+                    hintText: currentCurrency == null ? '' : '0',
+                    labelText: 'Initial balance'
+                  ),
+                ),),
+                new Padding(
+                  padding: new EdgeInsets.only(left: 10.0),
+                  child: new Builder(builder: (context) =>
+                  new FlatButton(
+                    child: new Text(
+                      currentCurrency == null
+                        ? 'Currency'
+                        : currentCurrency.shortName,
+                      style: new TextStyle(color: Colors.blue, fontSize: 18.0),
+                    ),
+                    onPressed: () {
+                      CurrencyProvider.getInstance().getAllCurrencies().then((
+                        currencies) =>
+                        showModalBottomSheet(
+                          context: context, builder: (context) =>
+                        new Column(
+                          children: <Widget>[
+                            new Expanded(child: new ListView(
+                              padding: new EdgeInsets.only(top: 10.0),
+                              children: currencies.map((currency) =>
+                              new ListTile(
+                                title: new Text(currency.longName),
+                                onTap: () {
+                                  setState(() {
+                                    currentCurrency = currency;
+                                    Navigator.pop(context);
+                                  });
+                                },
+                              )).toList(),
+                            ),)
+                          ],
+                        ))
+                      );
+                    },
+                  )),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
