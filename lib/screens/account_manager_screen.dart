3ff4ea7d90c192091bf04a51f18fed9747a06f51
ebolo <daothanhduy305@gmail.com>
2018-04-30 18:58:38 +0700

    Show dummy icon and account current balance info in the account list
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- lib/screens/account_manager_screen.dart
+++ lib/screens/account_manager_screen.dart
@@ -41,6 +41,8 @@ class AccountManagerScreenState extends State<AccountManagerScreen> {
 
   Widget buildAccountItemUI(Account account) =>
     new ListTile(
+      leading: new Icon(Icons.monetization_on),
       title: new Text(account.name),
+      subtitle: new Text('${account.currentBalance.toString()} (${account.currency.shortName})'),
     );
 }
