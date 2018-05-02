5e286442bc38c7ce93ea3a05642150d7f60dc245
ebolo <daothanhduy305@gmail.com>
2018-05-02 22:49:43 +0700

    Revamp new transaction screen ui
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- lib/screens/new_transaction_screen.dart
+++ lib/screens/new_transaction_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:monai/data/account.dart';
+import 'package:monai/data/currency.dart';
 
 class NewTransactionScreen extends StatefulWidget {
   @override
@@ -6,6 +8,17 @@ class NewTransactionScreen extends StatefulWidget {
 }
 
 class NewTransactionScreenState extends State<NewTransactionScreen> {
+  List<Account> currentAccounts = [];
+  Currency currentCurrency;
+  String transactionAmount = '0.0';
+
+  @override
+  void initState() {
+    super.initState();
+    AccountProvider.getInstance().getAllAccounts().then(
+        (accountList) => setState(() => currentAccounts.addAll(accountList)));
+  }
+
   @override
   Widget build(BuildContext context) => new Scaffold(
         appBar: new AppBar(
@@ -17,40 +30,105 @@ class NewTransactionScreenState extends State<NewTransactionScreen> {
         body: new ListView(
           children: <Widget>[
             new ListTile(
-              title: new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: 'Amount'),
+              leading: new Icon(Icons.monetization_on),
+              title: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(
+                          hintText: currentCurrency == null ? '' : '0',
+                          labelText: 'Transaction amount'),
+                      onChanged: (value) => transactionAmount = value,
+                    ),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.only(left: 10.0),
+                    child: new Builder(
+                        builder: (context) => new FlatButton(
+                              child: new Text(
+                                currentCurrency == null
+                                    ? 'Currency'
+                                    : currentCurrency.shortName,
+                                style: new TextStyle(
+                                    color: Colors.blue, fontSize: 18.0),
+                              ),
+                              onPressed: () {
+                                CurrencyProvider
+                                    .getInstance()
+                                    .getAllCurrencies()
+                                    .then((currencies) => showModalBottomSheet(
+                                        context: context,
+                                        builder: (context) => new Column(
+                                              children: <Widget>[
+                                                new Expanded(
+                                                  child: new ListView(
+                                                    padding:
+                                                        new EdgeInsets.only(
+                                                            top: 10.0),
+                                                    children: currencies
+                                                        .map((currency) =>
+                                                            new ListTile(
+                                                              title: new Text(
+                                                                  currency
+                                                                      .longName),
+                                                              onTap: () {
+                                                                setState(() {
+                                                                  currentCurrency =
+                                                                      currency;
+                                                                  Navigator.pop(
+                                                                      context);
+                                                                });
+                                                              },
+                                                            ))
+                                                        .toList(),
+                                                  ),
+                                                )
+                                              ],
+                                            )));
+                              },
+                            )),
+                  )
+                ],
               ),
             ),
             new ListTile(
+              leading: new Icon(Icons.category),
               title: new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: 'Currency'),
-              ),
-            ),
-            new ListTile(
-              title: new TextField(
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.text,
                 decoration: new InputDecoration(hintText: 'Category'),
               ),
             ),
             new ListTile(
+              leading: new Icon(Icons.edit),
               title: new TextField(
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.text,
                 decoration: new InputDecoration(hintText: 'Note'),
               ),
             ),
             new ListTile(
-              title: new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: 'Date time'),
-              ),
+              leading: new Icon(Icons.date_range),
+              title: new Theme(data: new ThemeData(
+                  disabledColor: Colors.black54
+              ), child: new TextField(
+                decoration: new InputDecoration(
+                    hintText: 'Date time'),
+                enabled: false,
+              )),
+              onTap: () {},
             ),
             new ListTile(
-              title: new TextField(
-                keyboardType: TextInputType.number,
-                decoration: new InputDecoration(hintText: 'Account'),
-              ),
+              leading: new Icon(Icons.account_balance_wallet),
+              title: new Theme(data: new ThemeData(
+                disabledColor: Colors.black54
+              ), child: new TextField(
+                decoration: new InputDecoration(
+                    hintText: currentAccounts.length > 0
+                        ? currentAccounts[0].name
+                        : 'Account'),
+                enabled: false,
+              )),
+              onTap: () {},
             ),
           ],
         ),
